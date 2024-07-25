523bba8d4ead7e09678dd9671d158fdf52a07f0e
Brochu <alexbrochu1@gmail.com>
2024-07-25 12:46:57 -0400

    [SETUP] Trying to link raylib to access some IO functions

--- pack.bat
+++ pack.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cl main.c -c -nologo -Od -Zi -TC -std:clatest
-link main.obj -WX -NOLOGO -DEBUG:FULL -PDB:umb.pdb -OUT:umb.exe
+cl main.c -c -nologo -Od -Zi -TC -std:clatest -I \include
+link main.obj libs\raylib.lib -WX -NOLOGO -DEBUG:FULL -PDB:umb.pdb -OUT:umb.exe
