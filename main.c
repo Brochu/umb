33afd729f8758ecf0c2ac15ad2a627b9abe30cbb
Brochu <alexbrochu1@gmail.com>
2024-07-25 13:09:55 -0400

    [DEV] Using raylib's functions to deal with simple first pass of init

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
 
-char *dir = NULL;
+#include "raylib.h"
+
+const char *dir = NULL;
 
 void umb_usage() {
     printf("usage: umb <command> [<args>]\n\n"
@@ -10,22 +13,16 @@ void umb_usage() {
     "\n");
 }
 
-int umb_init(char *p) {
-    if (p == NULL) {
-        DWORD len = 0;
-        len = GetCurrentDirectory(0, NULL);
+int umb_init(const char *p) {
+    dir = (p == NULL) ? GetWorkingDirectory() : p;
+    TraceLog(LOG_DEBUG, "[UMB] Calling 'init' command with <dir> = '%s'\n", dir);
 
-        dir = malloc(len); //TODO: Look into combining allocs in one arena?
-        GetCurrentDirectory(len, dir);
-    } else {
-        dir = p;
-    }
-
-    printf("[UMB] Calling 'init' command with <dir> = '%s'\n", dir);
     return EXIT_SUCCESS;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, const char **argv) {
+    SetTraceLogLevel(LOG_DEBUG);
+
     if (argc <= 1) {
         umb_usage();
     }
