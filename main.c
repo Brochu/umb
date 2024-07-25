0d42837a51dfa23491ad794423484878b9f6f175
Brochu <alexbrochu1@gmail.com>
2024-07-25 12:47:17 -0400

    [DEV] First pass on init command

--- main.c
+++ main.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 #include <windows.h>
 
-const char *path = NULL;
+char *dir = NULL;
 
-void usage() {
+void umb_usage() {
     printf("usage: umb <command> [<args>]\n\n"
     "commands: \n"
-    "\tinit -> Initializes directory for umb\n"
+    "\tinit [<dir>] -> Initializes <dir> directory for umb (will default to current directory)\n"
     "\n");
+}
+
+int umb_init(char *p) {
+    if (p == NULL) {
+        DWORD len = 0;
+        len = GetCurrentDirectory(0, NULL);
+
+        dir = malloc(len); //TODO: Look into combining allocs in one arena?
+        GetCurrentDirectory(len, dir);
+    } else {
+        dir = p;
+    }
+
+    printf("[UMB] Calling 'init' command with <dir> = '%s'\n", dir);
+    return EXIT_SUCCESS;
 }
 
 int main(int argc, char **argv) {
     if (argc <= 1) {
-        usage();
+        umb_usage();
     }
     else {
-        printf("Running exe: '%s'\n", argv[0]);
-
-        DWORD len = 0;
-        char dir[128];
-        len = GetCurrentDirectory(0, NULL);
-        GetCurrentDirectory(len, dir);
-        printf("PWD: '%s'\n", dir);
+        if (strcmp(argv[1], "init") == 0) {
+            if (argc == 2) {
+                return umb_init(NULL);
+            }
+            else {
+                return umb_init(argv[2]);
+            }
+        }
     }
+
+    return EXIT_FAILURE;
 }
